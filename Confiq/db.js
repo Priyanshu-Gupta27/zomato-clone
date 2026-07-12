38742a6cecd458cc8fe07ffb4e421e63ab3480e1
Priyanshu gupta <priyan969666@gmail.com>
2026-07-12 12:36:06 +0530

    final backend ready

--- Confiq/db.js
+++ Confiq/db.js
@@ -1,11 +1,15 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('food_db', 'root', 'priyan969666', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
- 
-
-module.exports = db;
+module.exports = sequelize;
